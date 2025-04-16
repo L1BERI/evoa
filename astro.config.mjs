70333093e9ef12fec3a6a69f357655c70803f3fd
unknown <kael1nvoker@mail.ru>
2025-04-16 15:46:45 +0300

    fixed pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-    site: 'https://l1beri.github.io',
-    base: 'evoa',
-  })
+export default defineConfig({})
